9ea0c85db27da31ecccc6ba46b8040ce2a1417ae
valentinmeto <0metov57@solent.ac.uk>
2025-11-08 15:33:11 +0000

    multiple_users2

--- week4.py
+++ week4.py
@@ -1,7 +1,24 @@
-def get_age():
-    print("Please enter your age")
-    age = int(input())
-    return age
+def sum_weight():
+    person = float(input("What is persons's weight?"))
+    inventory = float(input("What is the wight of the inventory?"))
+    total = person + inventory
+    return total
 
 
-print("Retrieved age", get_age())
+def cal_average(w1, w2):
+    return (w1 + w2) / 2
+
+
+def run():
+    choice = input("What would you like to do?")
+    if choice == "sum":
+        result = sum_weight()
+        print("The sum is: ", result)
+    elif choice == "cal average":
+        person = float(input("What is persons's weight?"))
+        inventory = float(input("What is wight of the inventory?"))
+        result = cal_average(person, inventory)
+        print("The average is: ", result)
+
+
+run()
