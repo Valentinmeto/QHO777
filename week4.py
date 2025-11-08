71c85238e247cefe40aa33d834160c6f9b7ff7bf
valentinmeto <0metov57@solent.ac.uk>
2025-11-08 15:01:31 +0000

    multiple_users1

--- week4.py
+++ week4.py
@@ -1,14 +1,7 @@
-def ladders_taken(jump):
-    print("how many steps are remaning?")
-    print (jump)
-    for i in range(jump):
-        print("| |")
-        print("***")
-print("###")
-print("$$$")
-def ask_draw():
-    values = int(input("How many steps has been taken? "))
-    ladders_taken(values)
+def get_age():
+    print("Please enter your age")
+    age = int(input())
+    return age
 
 
-ask_draw()
+print("Retrieved age", get_age())
